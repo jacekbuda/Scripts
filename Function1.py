e6cb91dc90ead2311420191aadffd9b3e4b7027c
Jacek Buda <jacek.buda123@gmal.com>
2018-09-04 18:50:09 +0200

    Next

--- Function1.py
+++ Function1.py
@@ -16,3 +16,11 @@ def solve_eq2(equationn):
     return 'x = ' + str(num2 -num1)
 print(solve_eq2('2 + x = 9'))
 
+def mult_divide(num1, num2):
+    if num1 > num2:
+        return (num1 * num2), (num1 /num2)
+
+mult, divide = mult_divide(5, 4)
+
+print('5 * 4 =', mult)
+print('5 / 4 =', divide)
