8ad5186d4b29f2c3721d7141752267becdf17b77
Jacek Buda <jacek.buda123@gmal.com>
2018-09-04 18:34:30 +0200

    Fresh

--- Function1.py
+++ Function1.py
@@ -7,3 +7,12 @@ def solve_eq(equation):
 
     return 'x = ' + str(num2 - num1)
 print(solve_eq('x + 4 = 9'))
+
+def solve_eq2(equationn):
+    num1, add, x, equal, num2 = equationn.split()
+
+    num1, num2, = int(num1), int(num2)
+
+    return 'x = ' + str(num2 -num1)
+print(solve_eq2('2 + x = 9'))
+
